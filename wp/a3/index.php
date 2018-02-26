b2fd406d145cfd428b79801b9f179a058a764725
elijah-g <elijah.glass@gmail.com>
2018-02-26 16:23:19 +0800

    pdated

--- wp/a3/index.php
+++ wp/a3/index.php
@@ -1,12 +1,13 @@
 <?php
 include ("tools.php");
-include_once("/home/eh1/e54061/public_html/wp/debug.php");
+
 top_module("MoonBoot");
 
 ?>
 
     <article id="about" class="centerblock container-fluid">
         <h2><b>ABOUT US</b></h2>
+        <?php include_once("/home/eh1/e54061/public_html/wp/debug.php"); ?>
         <p class='main'>Moonboot is a locally owned technology company in the heart of Bunbury. We pride ourselves in providing the highest quality service to all our clients, whilst maintaining the lowest possible prices. We strive to go above and beyond what is expected, to put a smile on your face.</p>
     </article>
 
